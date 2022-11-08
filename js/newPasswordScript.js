ecfcdc520c76e3599b7301cd3de5e420e83badf8
Mehmet Ali <kemalist611661@gmail.com>
2022-11-08 20:48:59 +0300

    consola gelen güvenlik kodu mesajı düzenlendi

--- js/newPasswordScript.js
+++ js/newPasswordScript.js
@@ -1,42 +1,55 @@
-let btnDOM = document.querySelector('#newPasswordBtn')
-let formDOM = document.querySelector('#userForm')
-let  titleDOM = document.querySelector('#title')
-let  inputDOM = document.querySelector('#input')
+let btnDOM = document.querySelector("#newPasswordBtn");
+let newBtnDOM = document.querySelector('#newBtn');
+let formDOM = document.querySelector("#userForm");
+let titleDOM = document.querySelector("#title");
+let inputDOM = document.querySelector("#input");
 
-
-formDOM.addEventListener('submit', formSubmit);
+formDOM.addEventListener("submit", formSubmit);
 
 function formSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 }
 
-function Code(safeCode){
-    this.safeCode = safeCode;
-}
-
-let code1 = new Code(baglantiGonder());
-
-btnDOM.addEventListener('click', baglantiGonder)
+btnDOM.addEventListener("click", baglantiGonder);
 function baglantiGonder() {
-    
-        let random = Math.floor(Math.random()*999999)
+  let random = Math.floor(Math.random() * 999999);
 
-        while(random < 100000) {
-            random = Math.floor(Math.random()*999999)
-        }
-    console.log(random)
+  while (random < 100000) {
+    random = Math.floor(Math.random() * 999999);
+  }
+  console.log(`Şifrenizi Değiştirmeniz İçin Altı Haneli Güvenlik Kodunuz : ${random}`);
 
+  btnDOM.remove();
+
+  let newBtn = document.createElement("button");
+  newBtn.setAttribute("id", "newBtn");
+  newBtn.innerText = "Doğrula";
+  newBtn.classList.add('btn');
+
+  let panelBtn = document.querySelector('#panelBtn')
+  panelBtn.appendChild(newBtn)
+
+  inputDOM.remove();
+
+  let newInp = document.createElement("input");
+  newInp.setAttribute("type","text")
+  newInp.setAttribute("id","newInp")
+  newInp.setAttribute("placeholder","Güvenlik Kodunu Giriniz")
+  newInp.setAttribute("size","31")
+  newInp.classList.add('input')
+
+  let panelInp = document.querySelector('#panelInput');
+  panelInp.appendChild(newInp);
+
+  
+
+  document.getElementById('newBtn').addEventListener('click', control => {
+     if(document.getElementById('newInp').value == random) {
+      prompt("Yeni Şifrenizi Giriniz : ")
+  }
+  })
 }
 
 
 
-function degisim() {
-    btnDOM.innerHTML = "Doğrula"
-    inputDOM.setAttribute("placeholder", "Güvenlik Kodunuz")  
-    inputDOM.setAttribute("type", "number")
-    
-    console.log(inputDOM.value)
-}
-
-console.log(code1.safeCode)
 
